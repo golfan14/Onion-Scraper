d7e46badc5d140ef9f2be0bf70ab15f9fb5b1783
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-06-22 09:36:21 -0600

    mongo connection

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ var morgan = require("morgan");
 
 var db = require("./models");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
 var PORT = 3000;
 
 var app = express();
